ecaaf0f120558fc1c9b2a43aafe6b3859ce2f3d1
Qayyax <qayyax@gmail.com>
2026-03-07 23:39:54 -0500

    tab button at the top, adjust the color

--- GroceryCalculator/Views/ListsView.swift
+++ GroceryCalculator/Views/ListsView.swift
@@ -10,28 +10,38 @@ import SwiftUI
 
 struct ListsView: View {
     
-    private var ListDemo: [GroceryList] = [
+    @State private var ListDemo: [GroceryList] = [
         GroceryList(title: "CVS Grocery list", date: specific),
         GroceryList(title: "Peter's Groceries", date: specific),
         GroceryList(title: "Farmer's market run", date: specific),
     ]
     
     var body: some View {
-        ZStack {
-            Color.primaryBg
-                .ignoresSafeArea()
-            VStack(alignment: .leading) {
-                NavigationStack {
-                   Text("List")
-                        .font(Font.largeTitle.bold())
-                    
+        NavigationStack {
+            ZStack {
+                Color.primaryBg
+                    .ignoresSafeArea()
+                VStack(alignment: .leading) {
                     Text("Search component goes here")
+                        .padding(.horizontal)
+                        .padding(.bottom, 24)
                     
                     // list of items goes here
                     List(ListDemo) { list in
                         GroceryListView(title: list.title, date: list.date)
                     }
                     Spacer()
+                }
+            }
+            .navigationTitle("Lists")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                       print("added")
+                    } label: {
+                        Image(systemName: "plus")
+                            .foregroundStyle(.accent)
+                    }
                 }
             }
         }
