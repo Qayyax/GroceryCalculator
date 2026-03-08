9a43af50f909429eeb8f9a29b0d377e89efd03ba
Qayyax <qayyax@gmail.com>
2026-03-07 22:10:01 -0500

    Initial Commit

---
+++ GroceryCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceryCalculator
+//
+//  Created by Abdul-Qayyum Olatunji on 2026-03-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceryCalculator/GroceryCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryCalculatorApp.swift
+//  GroceryCalculator
+//
+//  Created by Abdul-Qayyum Olatunji on 2026-03-07.
+//
+
+import SwiftUI
+
+@main
+struct GroceryCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
