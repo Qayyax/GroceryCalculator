983cb0ddd4d77dc128a51e2df4199b94ed2ee967
Qayyax <qayyax@gmail.com>
2026-03-23 18:13:19 -0400

    crated a new view

---
+++ GroceryCalculator/Components/NotesPerListView.swift
@@ -0,0 +1,18 @@
+//
+//  NotesPerListView.swift
+//  GroceryCalculator
+//
+//  Created by Abdul-Qayyum Olatunji on 2026-03-23.
+//
+
+import SwiftUI
+
+struct NotesPerListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotesPerListView()
+}
